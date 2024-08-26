d6a63c87a409c307a8398c137dcd5285b7aaa853
ericBlack1 <ericangeloawa@gmail.com>
2024-08-26 15:32:44 +0100

    added middleware

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userSchema.js');
+
+const authMiddleware = async (req, res, next) => {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId);
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
+  }
+};
+
+module.exports = authMiddleware;
