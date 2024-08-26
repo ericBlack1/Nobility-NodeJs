d78c2747767cad51dfd60c4fbbb122e9008b9587
Thierry <soyangthierry@ggmail.com>
2024-08-27 00:00:17 +0100

    backend more or less complete

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 
 connectDB();
-app.use(express.static('uploads'));
+app.use(express.static('./'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
