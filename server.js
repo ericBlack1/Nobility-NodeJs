4c27a34ec4593a52b422d578f707094a8fcde7a5
Thierry <soyangthierry@ggmail.com>
2024-08-26 23:12:09 +0100

    cors added

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
-const userRoutes = require('./routes/userRoutes.js');  // Corrected to plural
+const userRoutes = require('./routes/userRoutes.js');
 const profileRoutes = require('./routes/profileRoutes.js');
 const albumRoutes = require('./routes/albumRoutes.js');
 const songRoutes = require('./routes/songRoutes.js');
+const cors = require('cors'); // Import cors
 
 require('dotenv').config();
 
 const app = express();
 
+// Enable CORS for all origins
+app.use(cors());
+
 connectDB();
-app.use(express.static(`uploads`))
+app.use(express.static('uploads'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -19,7 +23,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/profiles', profileRoutes);
 app.use('/api/albums', albumRoutes);
 app.use('/api/songs', songRoutes);
-
 
 const PORT = process.env.PORT || 5000;
 
